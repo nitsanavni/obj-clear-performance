a953899b38ad26869a55f7b28499202895b2736e
Nitsan Avni <nitsan@papayaglobal.com>
2018-12-09 21:18:40 +0200

    add specs

---
+++ spec/perf.spec.js
@@ -0,0 +1,26 @@
+describe('test object clearing performance', () => {
+
+    it('new obj each time', () => {
+        let before = new Date();
+        for (let j = 0; j < 1000; j++) {
+            let obj = {};
+            for (let i = 0; i < 100000; i++) {
+                obj[i] = i;
+            }
+        }
+
+        console.log('new obj took', new Date() - before)
+    });
+
+    it('same obj each time', () => {
+        let before = new Date();
+        let obj = {};
+        for (let j = 0; j < 1000; j++) {
+            for (let i = 0; i < 100000; i++) {
+                obj[i] = i;
+            }
+        }
+        console.log('same obj took', new Date() - before)
+    });
+
+});
